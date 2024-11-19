25557d9d041735ffbfc72067122b4de7ace84d5f
DinnerMaster <Antlion2021@yandex.com>
2024-11-19 16:39:09 +0800

    RPN forward method

--- Main/RPN.py
+++ Main/RPN.py
@@ -5,7 +5,7 @@ import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class RPN(nn.Module):
+class RPN(nn.Module):  # R-CNN RPN part: First Layer
     def __init__(self, in_channels=512):
         super(RPN, self).__init__()
         self.scales = [128, 256, 512]
@@ -18,13 +18,20 @@ class RPN(nn.Module):
                                   kernel_size=3,
                                   stride=1,
                                   padding=1)
-        # 1*1 conv:classification
+        # 1*1 Classification
         self.cls_layer = nn.Conv2d(in_channels,
                                    self.num_anchors,
                                    kernel_size=1,
                                    stride=1)
-        # 1*1 regression
+        # 1*1 Box regression
         self.bbox_reg_layer = nn.Conv2d(in_channels,
                                         self.num_anchors * 4,
                                         kernel_size=1,
                                         stride=1)
+
+    def forward(self, image, feat, target):
+        # Call RPN Layer
+        rpn_feat = nn.ReLU()(self.rpn_conv(feat))
+        cls_score = self.cls_layer(rpn_feat)
+        bbox_transform_pred = self.bbox_reg_layer(rpn_feat)
+        # Generate Anchor
